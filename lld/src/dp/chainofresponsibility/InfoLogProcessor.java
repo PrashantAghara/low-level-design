58bf47a20b0e3b81178e3e9a89e707556b6229cb
Prashant Aghara <prashant.aghara2111@gmail.com>
2025-09-09 13:24:58 +0530

    Chain of Responsibility D.P.

---
+++ lld/src/dp/chainofresponsibility/InfoLogProcessor.java
@@ -0,0 +1,15 @@
+package dp.chainofresponsibility;
+
+public class InfoLogProcessor extends LogProcessor {
+    InfoLogProcessor(LogProcessor loggerProcessor) {
+        super(loggerProcessor);
+    }
+
+    public void log(int logLevel, String message) {
+        if (logLevel == INFO) {
+            System.out.println("INFO: " + message);
+        } else {
+            super.log(logLevel, message);
+        }
+    }
+}
